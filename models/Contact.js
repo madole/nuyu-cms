698755e8f5a384b60c68c4fa0f03aa94501b5a2f
Andrew McDowell <madoliole@gmail.com>
2015-07-12 17:50:20 +1000

    adding a modal lightbox gallery... temporary

--- models/Contact.js
+++ models/Contact.js
@@ -4,9 +4,11 @@ var Types = keystone.Field.Types;
 var Contact = new keystone.List('Contact', {label: 'Contact Details'});
 
 Contact.add({
-	name: {type: Types.Text, default: 'Contact Details'},
+	name: {type: Types.Text},
 	email: {type: Types.Email},
-	telephone: {type: Types.Text}
+	telephone: {type: Types.Text},
+	address: {type: Types.Textarea},
+	website: {type: Types.Url}
 });
 
 Contact.register();
